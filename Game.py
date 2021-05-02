cb0f72d29c98cb0e4e2f40226cfb13f8224b4f13
Alexander <alex_alegavr@mail.ru>
2021-05-02 04:22:26 +0300

    added GUI, and Facade pattern

--- Game.py
+++ Game.py
@@ -12,9 +12,17 @@ class Game:
     def __init__(self):
         self.weapon = "finger"
         self.score = 0
-        self.name = input("Enter your name: ")
+        self.name = "Alex"
         self.snakes = []
         self.fruits = []
+        self.fruits.append(generate_fruit(choice(Globals.fruits)))
+        self.fruits.append(generate_fruit(choice(Globals.fruits)))
+
+        self.fruits.append(generate_fruit(choice(Globals.fruits)))
+
+        snake = Snake([Point(1, 1), Point(1,2), Point(2,2)], "red")
+        self.snakes.append(snake)
+        self.snakes.append(Snake([Point(5, 1), Point(5,2), Point(6,2)], "red"))
         
     def display_objects(self):
         field = [["_"]*Globals.field_size for i in range(Globals.field_size)]
@@ -28,31 +36,20 @@ class Game:
             print(*field[i])
 
     def run(self):
-        self.fruits.append(generate_fruit(choice(Globals.fruits)))
-        self.fruits.append(generate_fruit(choice(Globals.fruits)))
+        for snake in self.snakes:
+            snake.next_position()
+            i = 0
+            while i < len(self.fruits):
+                if snake.coordinates[0] == self.fruits[i].coordinates:
+                    if str(self.fruits[i]) == "Apple":
+                        self.snakes.append(copy.deepcopy(snake))
+                    elif str(self.fruits[i]) == "Pineapple":
+                        snake.take_bonus()
+                    self.fruits.pop(i)
+                    self.fruits.append(generate_fruit(choice(Globals.fruits)))
 
-        self.fruits.append(generate_fruit(choice(Globals.fruits)))
-
-        snake = Snake([Point(1, 1), Point(1,2), Point(2,2)], "red")
-        self.snakes.append(snake)
-        self.snakes.append(Snake([Point(5, 1), Point(5,2), Point(6,2)], "red"))
-
-        while True:
-            self.display_objects()
-            for snake in self.snakes:
-                snake.next_position()
-                i = 0
-                while i < len(self.fruits):
-                    if snake.coordinates[0] == self.fruits[i].coordinates:
-                        if str(self.fruits[i]) == "Apple":
-                            self.snakes.append(copy.deepcopy(snake))
-                        elif str(self.fruits[i]) == "Pineapple":
-                            snake.take_bonus()
-                        self.fruits.pop(i)
-                        self.fruits.append(generate_fruit(choice(Globals.fruits)))
-
-                    else:
-                        i += 1
+                else:
+                    i += 1
 
             sleep(0.1)
             os.system("clear")
---
+++ tkin.py
@@ -0,0 +1,76 @@
+import tkinter as tk
+from Globals import Globals
+from Game import Game
+from Point import Point
+
+
+class Drawer():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("440x440")
+        self.game = Game()
+        self.canvas = tk.Canvas()
+        self.draw()
+        
+        self.root.bind('<Button-1>', self.kill_snake)
+        
+        self.root.mainloop()
+
+
+    def kill_snake(self, event):
+        x = event.x // Globals.field_size - 1
+        y = event.y // Globals.field_size - 1
+        i = 0
+        while i < len(self.game.snakes):
+            if self.game.snakes[i].coordinates[0] == Point(x, y):
+                del self.game.snakes[i]
+                self.game.score += 100
+                if len(self.game.snakes) == 0:
+                    print(f"YOU WON! YOUR SCORE: {self.game.score}")
+                    exit()
+            else:
+                i += 1
+
+
+    def draw(self):
+        self.game.run()
+        self.canvas.delete("all")
+        for i in range(1, 21):
+            for j in range(1, 21):
+                self.canvas.create_rectangle(i*Globals.field_size, 
+                                        j*Globals.field_size, 
+                                        i*Globals.field_size+20, 
+                                        j*Globals.field_size+20, 
+                                        fill="white", 
+                                        outline="black")
+        for snake in self.game.snakes:
+            for coord in snake.coordinates:
+                self.canvas.create_rectangle((coord.x+1)*Globals.field_size, 
+                                        (coord.y+1)*Globals.field_size, 
+                                        (coord.x+1)*Globals.field_size+20, 
+                                        (coord.y+1)*Globals.field_size+20, 
+                                        fill="blue", 
+                                        outline="black")
+            coord = snake.coordinates[0]
+            self.canvas.create_rectangle((coord.x+1)*Globals.field_size, 
+                                    (coord.y+1)*Globals.field_size, 
+                                    (coord.x+1)*Globals.field_size+20, 
+                                    (coord.y+1)*Globals.field_size+20, 
+                                    fill="green", 
+                                    outline="black")
+        for fruit in self.game.fruits:
+            coord = fruit.coordinates
+            self.canvas.create_rectangle((coord.x+1)*Globals.field_size, 
+                                    (coord.y+1)*Globals.field_size, 
+                                    (coord.x+1)*Globals.field_size+20, 
+                                    (coord.y+1)*Globals.field_size+20, 
+                                    fill=fruit.color, 
+                                    outline="black")
+        self.canvas.pack(fill=tk.BOTH, expand=1)
+        self.root.after(100, self.draw)
+
+    
+
+
+if __name__ == '__main__':
+    Drawer()
